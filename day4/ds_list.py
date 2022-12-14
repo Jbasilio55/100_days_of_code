6bf8ca457a0d500c4f02e2956636070e18a54055
Jbasilio55 <j.l.basilio55@gmail.com>
2022-12-14 10:51:47 -0500

    completed code challenge 2 - day 4

---
+++ day4/ds_list.py
@@ -0,0 +1,15 @@
+states_of_america = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado",
+"Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois",
+"Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland",
+"Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana",
+"Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York",
+"North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania",
+"Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah",
+"Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
+
+print(states_of_america[23])
+print(len(states_of_america))
+states_of_america[0] = "Alabama..."
+states_of_america.append("Jorge-ville")
+states_of_america.extend([1, 2, 3])
+print(states_of_america)
